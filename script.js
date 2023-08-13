6b8ce3aab4e3ed46c01d7916066b86656ff08b4b
Roman Prusakov <prusakovr073@gmail.com>
2023-08-13 21:59:07 +0300

    realized addition to LS

--- script.js
+++ script.js
@@ -1,25 +1,9 @@
 const cross = document.querySelector('.cross');
 const addInput = document.querySelector('.add input');
 
-function addTask() {
-  // let value = this.parentElement.querySelector('input').value;
-  let value = addInput.value;
+function renderItem(markup, outputClass) {
 
-  let li = `<li class="rounded">
-  <span>${value}</span>
-  <div class="btn-actions">
-    <a href="" class="text-white btn-important me-1 d-inline-block">
-      <i class="bi bi-patch-exclamation fs-5"></i>
-    </a>
-    <a href="" class="text-white btn-delete">
-      <i class="bi bi-x fs-3"></i>
-    </a>
-  </div>
-</li>`;
-
-  document.querySelector('.list').insertAdjacentHTML('beforeend', li);
-
-  clearInput();
+  document.querySelector(outputClass).insertAdjacentHTML('beforeend', markup);
 
 }
 
@@ -27,8 +11,8 @@ function removeActiveCrossClass() {
   cross.classList.remove('cross-active');
 }
 
-function clearInput() {
-  addInput.value = '';
+function clearInput(input) {
+  input.value = '';
 
   removeActiveCrossClass();
 }
@@ -37,19 +21,74 @@ function showCross() {
   addInput.value.trim() ? cross.classList.add('cross-active') : removeActiveCrossClass();
 }
 
-document.querySelector('.btn-add').addEventListener('click', function() {
-  if (addInput.value.trim()) { 
-    addTask();
+function addItemToLS(name, source, params = null) {
+  let items = JSON.parse(localStorage.getItem(source)) || [];
+
+  let newItem;
+
+  if (items.length == 0) {
+    newItem = { id: 1, name, ...params };
+  } else {
+    const id = items[items.length - 1].id + 1;
+    newItem = { id, name, ...params };
   }
+
+  items.push(newItem);
+
+  localStorage.setItem(source, JSON.stringify(items));
+}
+
+document.querySelector('.btn-add').addEventListener('click', function(event) {
+  const input = event.target.parentElement.querySelector('.add input');
+  const li = `<li class="rounded">
+    <span>${input.value}</span>
+    <div class="btn-actions">
+      <a href="" class="text-white btn-important me-1 d-inline-block">
+        <i class="bi bi-patch-exclamation fs-5"></i>
+      </a>
+      <a href="" class="text-white btn-delete">
+        <i class="bi bi-x fs-3"></i>
+      </a>
+    </div>
+  </li>`;
+
+  if (addInput.value.trim()) { 
+    addItemToLS(input.value, 'tasks', { important: false, done: false });
+
+    renderItem(li, '.list');
+
+    clearInput(input);
+  }
+
 });
+
 document.addEventListener('keydown', function(event) {
+  const input = event.target.parentElement.querySelector('.add input');
+  const li = `<li class="rounded">
+    <span>${input.value}</span>
+    <div class="btn-actions">
+      <a href="" class="text-white btn-important me-1 d-inline-block">
+        <i class="bi bi-patch-exclamation fs-5"></i>
+      </a>
+      <a href="" class="text-white btn-delete">
+        <i class="bi bi-x fs-3"></i>
+      </a>
+    </div>
+  </li>`;
+
   if ((event.keyCode == 13 || event.keyCode == 9) && addInput.value.trim()) {
-    addTask();
+    addItemToLS(input.value, 'tasks', { important: false, done: false });
+
+    renderItem(li, '.list');
+
+    clearInput(input);
   }
 });
 
 addInput.addEventListener('input', showCross);
-document.querySelector('.cross').addEventListener("click", clearInput);
+
+document.querySelector('.cross').
+addEventListener("click", clearInput);
 
 
 
