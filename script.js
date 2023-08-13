da98e8446541dc7978f06e38ce302d2087e02c64
Roman Prusakov <prusakovr073@gmail.com>
2023-08-13 09:35:58 +0300

    appended some stuff to addTask part

--- script.js
+++ script.js
@@ -1,25 +1,55 @@
+const cross = document.querySelector('.cross');
+const addInput = document.querySelector('.add input');
 
 function addTask() {
-  // let value = this.previousElementSibling.value;
-  let value = this.parentElement.querySelector('input').value;
+  // let value = this.parentElement.querySelector('input').value;
+  let value = addInput.value;
 
-  let li = `<li class="list-group-item">
+  let li = `<li class="rounded">
   <span>${value}</span>
   <div class="btn-actions">
-    <a href="" class="text-white btn-important me-1">
-      <i class="bi bi-exclamation-circle-fill fs-5"></i>
+    <a href="" class="text-white btn-important me-1 d-inline-block">
+      <i class="bi bi-patch-exclamation fs-5"></i>
     </a>
     <a href="" class="text-white btn-delete">
-      <i class="bi bi-x-circle-fill fs-5"></i>
+      <i class="bi bi-x fs-3"></i>
     </a>
   </div>
-  </li>`;
+</li>`;
 
   document.querySelector('.list').insertAdjacentHTML('beforeend', li);
 
+  clearInput();
+
 }
 
-document.querySelector('.btn-add').addEventListener('click', addTask);
+function removeActiveCrossClass() {
+  cross.classList.remove('cross-active');
+}
+
+function clearInput() {
+  addInput.value = '';
+
+  removeActiveCrossClass();
+}
+
+function showCross() {
+  addInput.value.trim() ? cross.classList.add('cross-active') : removeActiveCrossClass();
+}
+
+document.querySelector('.btn-add').addEventListener('click', function() {
+  if (addInput.value.trim()) { 
+    addTask();
+  }
+});
+document.addEventListener('keydown', function(event) {
+  if ((event.keyCode == 13 || event.keyCode == 9) && addInput.value.trim()) {
+    addTask();
+  }
+});
+
+addInput.addEventListener('input', showCross);
+document.querySelector('.cross').addEventListener("click", clearInput);
 
 
 
---
+++ vanilla-tilt.js
@@ -0,0 +1,8 @@
+VanillaTilt.init(document.querySelectorAll(".wrapper"), {
+  max: 0,
+  speed: 500,
+  easing:"cubic-bezier(.03,.98,.52,.99)",
+  transition:true,
+  glare: true,
+  "max-glare": 0.05
+});
